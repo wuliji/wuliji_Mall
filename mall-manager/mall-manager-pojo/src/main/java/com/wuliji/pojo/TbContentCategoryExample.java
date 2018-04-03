41ca5bff3bbe70a04f28414a7f15fd234ceecfbf
wuliji <Uk_wuliji@163.com>
2018-04-03 19:15:22 +0800

    工程拆分：Web层与服务层拆分，Dubbo与Zookeeper结合，多台tomcat服务器结合。

--- mall-manager/mall-manager-pojo/src/main/java/com/wuliji/pojo/TbContentCategoryExample.java
+++ mall-manager/mall-manager-pojo/src/main/java/com/wuliji/pojo/TbContentCategoryExample.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class TbContentCategoryExample {
+public class TbContentCategoryExample{
     protected String orderByClause;
 
     protected boolean distinct;
--- mall-manager/mall-manager-pojo/src/main/java/com/wuliji/pojo/TbContentExample.java
+++ mall-manager/mall-manager-pojo/src/main/java/com/wuliji/pojo/TbContentExample.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class TbContentExample {
+public class TbContentExample{
     protected String orderByClause;
 
     protected boolean distinct;
